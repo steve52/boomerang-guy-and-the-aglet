bb3cc10c1d78bc56a6caecd57d857ba909e327c9
Firoty1 <ultrapokegamer55@gmail.com>
2026-08-25 14:53:01 -0400

    player look good

--- player.gd
+++ player.gd
@@ -2,14 +2,17 @@ extends CharacterBody2D
 
 @export var Speed = 20
 const BOOMERANG = preload("uid://bgv7yok8kgxfo")
-
+var Walking = false
+var Rotate = 0
 
 func _physics_process(delta):
-	#Movement
+	
 	if Input.is_action_pressed("Down"):
 		velocity.y = Speed * delta * 1000
+		$AnimatedSprite2D.animation = "Front"
 	elif Input.is_action_pressed("Up"):
 		velocity.y = -Speed * delta * 1000
+		$AnimatedSprite2D.animation = "Back"
 	else:
 		velocity.y = 0
 	if Input.is_action_pressed("Left"):
@@ -19,7 +22,21 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 	
-	#Attacking
+	if velocity != Vector2(0,0):
+		Walking = true
+	else:
+		Walking = false
+	
+	if Walking:
+		if Rotate == 0:
+			rotation_degrees += 2
+			if rotation_degrees >= 15:
+				Rotate = 1
+		else:
+			rotation_degrees -= 2
+			if rotation_degrees <= -15:
+				Rotate = 0
+	
 	if Input.is_action_just_pressed("Attack"):
 		Attack()
 	
