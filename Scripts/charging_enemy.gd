0628aa5fc8ef9968b1d6ec98b2963f8f7d8294bb
Firoty1 <ultrapokegamer55@gmail.com>
2026-08-25 18:01:06 -0400

    smallfixestomakebetter
    randomize charge time
    max of 3 boomerangs

--- Scripts/charging_enemy.gd
+++ Scripts/charging_enemy.gd
@@ -2,20 +2,25 @@ extends "res://Scripts/EnemyClass.gd"
 
 const CHARGING_ENEMY_BULLETS = preload("uid://ctd6ht4ofjbmv")
 
+func _ready():
+	super()
+	$ChargeTimer.start(3+randf_range(-1,1))
+
+
 func BeforeCharge():
 	var tween = get_tree().create_tween()
 	var tilt = -15 if Player.position.x > position.x else 15
 	tween.tween_property(self, "rotation_degrees", tilt, .5) 
 	await get_tree().create_timer(.6).timeout
-	for i in 8:
-		var BulletDirection = Vector2.from_angle(deg_to_rad(i*45))
+	for i in 4:
+		var BulletDirection = Vector2.from_angle(deg_to_rad(i*90+45))
 		ShootBullets(CHARGING_ENEMY_BULLETS, BulletDirection)
 	Charge()
 
 func  AfterCharge():
-	for i in 8:
-		var BulletDirection = Vector2.from_angle(deg_to_rad(i*45))
+	for i in 4:
+		var BulletDirection = Vector2.from_angle(deg_to_rad(i*90))
 		ShootBullets(CHARGING_ENEMY_BULLETS, BulletDirection)
-	$ChargeTimer.start()
+	$ChargeTimer.start(3+randf_range(-1,1))
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "rotation_degrees", 0, .2)
