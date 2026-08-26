3e530e517679ef1e9af40a59df7b7a0ae796f34b
Firoty1 <ultrapokegamer55@gmail.com>
2026-08-26 14:58:55 -0400

    teleportingenemynearlydone

--- Scripts/teleporting_enemy.gd
+++ Scripts/teleporting_enemy.gd
@@ -1,6 +1,6 @@
 extends "res://Scripts/EnemyClass.gd"
 
-var ShotsToTeleport = 3
+var ShotsToTeleport = 2
 const TELEPORTING_ENEMY_HELIX_BULLETS = preload("uid://syjmblse5q32")
 
 
@@ -12,3 +12,18 @@ func _on_helix_shot_timer_timeout():
 		await get_tree().create_timer(.2).timeout
 		ShootBullets(TELEPORTING_ENEMY_HELIX_BULLETS, Target)
 	$HelixShotTimer.start(1.5+randf_range(-.5,.5))
+	ShotsToTeleport -= 1
+	if ShotsToTeleport == 0:
+		var GoodTeleport = false
+		var TeleportTarget: Vector2
+		while !GoodTeleport:
+			TeleportTarget = Vector2(randf_range(-320,320),randf_range(-320,320))
+			$TeleportCheck.target_position = TeleportTarget
+			print("yay")
+			$TeleportCheck.force_raycast_update()
+			if $TeleportCheck.is_colliding() == false:
+				Teleport(TeleportTarget + position)
+				print("YAY")
+				GoodTeleport = true
+		
+		ShotsToTeleport = 2
