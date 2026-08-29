5d7eff4faf942212619a1493ec38dd69ab732b98
Steve Giordano <steve1nyc@gmail.com>
2026-08-29 08:43:52 -0400

    Add spawn points echo "Good Morning " + $USER

--- Scripts/room_area_2d.gd
+++ Scripts/room_area_2d.gd
@@ -1,12 +1,10 @@
 extends Area2D
 
 func isEnemy(body: Node2D) -> bool:
-	return body.is_in_group("enemies")
+	return body is EnemySpawnPoint
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("player")):
-		var enemies = get_overlapping_bodies().filter(isEnemy)
-		print('~~~', enemies)
-		for enemy in enemies:
-			print('~~~', enemy)
-			enemy.wakeUp()
+		var enemy_spawn_points = get_overlapping_areas().filter(isEnemy)
+		for spawn_point in enemy_spawn_points:
+			spawn_point.spawn()
